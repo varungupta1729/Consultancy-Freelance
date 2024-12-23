eb1bca3456e74f1e322dc3dc853eb3d6b91a7923
Varun Gupta <varungupta1228@gmail.com>
2024-12-24 01:26:50 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,72 @@
+export const bigProjects = {
+    title: "Big Projects",
+    subtitle: "Full Stack MERN Projects",
+    projects: [
+      {
+        image: "https://we-designe-tees.vercel.app/static/media/squarelogo.b99fee2a02ada163fec1.png",        
+        projectName: "WeDesigneTees",
+        projectDesc: "Full Stack Tshirt purchasing website",
+        footerLink: [
+          {
+            name: "Visit Website",
+            url: "https://we-designe-tees.vercel.app/"
+          },
+          {
+            name: "Github",
+            url: "https://github.com/varungupta1729/WeDesigneTees"
+          },{
+            name: "Video",
+            url: "https://www.youtube.com/watch?v=vxWTz7_SAVI&feature=youtu.be"
+          }
+          //  you can add extra buttons here.
+        ]
+      },
+      {
+        // image: require("./assets/images/Logo-Full-Dark.png"),
+        // imageDark: require("./assets/images/Logo-Full-Light.png"),
+        projectName: "KIET",
+        projectDesc: "Students course purchasing website ",
+        footerLink: [
+          {
+            name: "Visit Website",
+            url: "https://www.kiet.edu/"
+          },  {
+            name: "Github",
+            url: "https://github.com/varungupta1729/EdTech-Platform"
+          }
+        ]
+      },
+      {
+        // image: "https://seekvectors.com/files/download/Black%20Color%20Netflix-01.png",
+        // imageDark:"https://seekvectors.com/files/download/Netflix-Logo-03.png",
+        projectName: "NetflixGPT",
+        projectDesc: "Watch Movies with AI assistance",
+        footerLink: [
+          {
+            name: "Visit Website",
+            url: "https://netflix-gpt-seven-opal.vercel.app/"
+          },  {
+            name: "Github",
+            url: "https://github.com/varungupta1729/Netflix-GPT"
+          }
+        ]
+      },
+      {
+        // image: require("./assets/images/DesignerStudio_Black.png"),
+        // imageDark: require("./assets/images/DesignerStudio_WHite.png"),
+        projectName: "DesignerStudio",
+        projectDesc: "Design your Clothes with AI",
+        footerLink: [
+          {
+            name: "Visit Website",
+            url: "https://designer-studio.vercel.app/"
+          },  {
+            name: "Github",
+            url: "https://github.com/varungupta1729/DesignerStudio"
+          }
+        ]
+      }
+    ],
+    display: true // Set false to hide this section, defaults to true
+  };
+  
